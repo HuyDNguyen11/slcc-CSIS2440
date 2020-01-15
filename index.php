865c84bbd5ad089d5f07aef8a61ac9d13ae8ecae
lordndhuy <lordndhuy@gmail.com>
2020-01-15 10:24:23 -0700

    add composer.json

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello world</title>
+        <?php
+            $name = "Huy D. Nguyen";
+            $age = 23;
+        ?>
+    </head>
+    <body>
+        <?php
+            echo "<h1>This is</h1>\n";
+            print("$name age:  $age ");
+
+        ?>
+    </body>
+</html>
