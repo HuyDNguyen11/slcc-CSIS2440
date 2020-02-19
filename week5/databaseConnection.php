059feead2f71f94e4de4eebf102f7aba2b9ba0f6
lordndhuy <lordndhuy@gmail.com>
2020-02-19 13:44:58 -0700

    w5update

--- week5/databaseConnection.php
+++ week5/databaseConnection.php
@@ -3,6 +3,13 @@ $host = 'localhost';
 $user = 'HuyDNguyen';
 $password = 'Huynguyen1';
 $dbname = 'CSIS2440';
-$conn = new mysqli($host, $user, $password, $dbname);
-print_r($conn);
+$conn = new mysqli($host, $user, $password);
+$conn->query("USE CSIS2440;");
+
+$res = $conn->query("show columns from Planets;");
+$row = $res->fetch_assoc();
+print_r($row);
+$row = $res->fetch_assoc();
+print_r($row);
+s
 ?>
