1102e146c726d9b9d9f81ba68b3fa2fc838daba0
lordndhuy <lordndhuy@gmail.com>
2020-02-19 14:08:33 -0700

    w5update

---
+++ database.php
@@ -0,0 +1,24 @@
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'    => '',
+    'hostname' => 'us-cdbr-iron-east-04.cleardb.net',
+    'username' => 'bc5308ef732f00',
+    'password' => '9dbbb8d8',
+    'database' => '',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
