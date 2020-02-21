29245bcb18f3289cae258c2d914ef24439859dc2
lordndhuy <lordndhuy@gmail.com>
2020-02-20 23:05:11 -0700

    update

--- week5/databaseConnection.php
+++ week5/databaseConnection.php
@@ -3,13 +3,11 @@ $host = 'us-cdbr-iron-east-04.cleardb.net';
 $user = 'bc5308ef732f00';
 $password = '9dbbb8d8';
 $dbname = 'heroku_521233846929941';
-$conn = new mysqli($host, $user, $password);
-$conn->query("USE $dbname;");
-
-$res = $conn->query("show columns from planets;");
-$row = $res->fetch_assoc();
-print_r($row);
-$row = $res->fetch_assoc();
-print_r($row);
-
+$conn = new mysqli($host, $user, $password, $dbname);
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "<b><h2>We Connected!</h2></b>";
+print_r($conn);
+$conn->close();
 ?>
