3a1194928eeac4b4d93cf8eedb94c61fbc9991e1
lordndhuy <lordndhuy@gmail.com>
2020-02-20 22:39:41 -0700

    w5update

--- week5/databaseConnection.php
+++ week5/databaseConnection.php
@@ -1,15 +1,15 @@
 <?php
-$host = 'localhost';
-$user = 'HuyDNguyen';
-$password = 'Huynguyen1';
-$dbname = 'CSIS2440';
+$host = 'us-cdbr-iron-east-04.cleardb.net';
+$user = 'bc5308ef732f00';
+$password = '9dbbb8d8';
+$dbname = 'heroku_521233846929941';
 $conn = new mysqli($host, $user, $password);
-$conn->query("USE CSIS2440;");
+$conn->query("USE $dbname;");
 
-$res = $conn->query("show columns from Planets;");
+$res = $conn->query("show columns from planets;");
 $row = $res->fetch_assoc();
 print_r($row);
 $row = $res->fetch_assoc();
 print_r($row);
-s
+
 ?>
